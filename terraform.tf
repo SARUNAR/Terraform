ccf7d29b7077e386469e419527e95ef982fe1164
anoop <anooppalavai2424@gmail.com>
2018-01-11 14:19:21 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
-provider "aws" {
-       access_key = "${var.access_key}"
-       secret_key = "${var.secret_key}"
-       region = "us-east-1"
-   }
+//provider "aws" {
+  //     access_key = "${var.access_key}"
+    //   secret_key = "${var.secret_key}"
+      // region = "us-east-1"
+   //}
 
 resource "aws_instance" "example"  {
         ami = "ami-55ef662f"
